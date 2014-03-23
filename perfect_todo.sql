23a91ffe637726979f53016bb3afd0622ccbc761
EgamiYusuke <egpersent@gmail.com>
2014-03-23 19:28:38 +0900

    プロジェクトテーブル追加　外部キー追加

--- perfect_todo.sql
+++ perfect_todo.sql
@@ -1,38 +1,36 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- データベース: `perfect_todo`
---
+SET time_zone = "+09:00";
 CREATE DATABASE `perfect_todo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `perfect_todo`;
 
--- --------------------------------------------------------
-
---
--- テーブルの構造 `tasks`
---
-
-DROP TABLE IF EXISTS `tasks`;
-CREATE TABLE IF NOT EXISTS `tasks` (
+DROP TABLE IF EXISTS `projects`;
+CREATE TABLE IF NOT EXISTS `projects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `t_title` text NOT NULL,
-  `t_content` text,
-  `t_limit` datetime DEFAULT NULL,
-  `t_size` int(11) NOT NULL COMMENT '0:long/1:middle/2:short',
+  `p_title` text NOT NULL,
+  `p_content` text,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   `del_flg` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `id_idx` (`user_id`)
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- テーブルの構造 `users`
---
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_id` int(11) NOT NULL,
+  `t_title` text NOT NULL,
+  `t_content` text,
+  `t_limit` datetime DEFAULT NULL,
+  `t_size` enum('long','middle','short') NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `del_flg` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `id_idx` (`task_id`),
+  KEY `task_id` (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
@@ -49,12 +47,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `mail_UNIQUE` (`mail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- ダンプしたテーブルの制約
---
 
---
--- テーブルの制約 `tasks`
---
+ALTER TABLE `projects`
+  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
 ALTER TABLE `tasks`
-  ADD CONSTRAINT `id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `projects` (`id`);
