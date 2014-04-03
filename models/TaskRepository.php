99091b67512cff5d101ced098b56b99ecb4d904a
takobonge <shungo.ishino+takobonge@gmail.com>
2014-04-03 23:58:18 +0900

    LEFT JOINのとこ

--- models/TaskRepository.php
+++ models/TaskRepository.php
@@ -8,19 +8,16 @@
 class TaskRepository extends DbRepository
 {
     public function fetchAllTasksByUserId($user_id) {
-        $sql = "SELECT
-                t.*,
-                p.id as p_id,
-                p.p_title as p_title,
-                u.id as u_id,
-                u.username as username
-            FROM tasks t
-            LEFT JOIN projects p
-                ON p.id = t.project_id
-            LEFT JOIN users u
-                ON p.user_id = :user_id
-                WHERE t.del_flg = '0'
-            ORDER BY t.created DESC";
+        $sql = "SELECT t.*,
+                       p.id as p_id,
+                       p.p_title as p_title,
+                       u.id as u_id,
+                       u.username as username
+                    FROM tasks t
+                        LEFT JOIN projects p ON p.id = t.project_id
+                        LEFT JOIN users u ON p.user_id = u.id
+                    WHERE p.user_id = :user_id and t.del_flg = '0'
+                    ORDER BY t.created DESC";
 
         return $this->fetchAll($sql, array(':user_id' => $user_id));
     }
@@ -52,12 +49,7 @@ class TaskRepository extends DbRepository
 
     public function delete($task_id) {
         $now = new DateTime();
-        $sql = "
-            UPDATE tasks SET
-                del_flg = '1',
-                modified = ?
-                    WHERE id = ?
-            ";
+        $sql = "UPDATE tasks SET del_flg = '1', modified = ? WHERE id = ?";
 
         $stmt = $this->execute($sql, array(
             $now->format('Y-m-d H:i:s'),
